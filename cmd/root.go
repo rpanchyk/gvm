090786d24bee3fc364e5a70374937b461c5b61a4
rpanchyk <ruslan.panchik@gmail.com>
2024-05-06 14:47:56 +0300

    Add current dir for config file search

--- cmd/root.go
+++ cmd/root.go
@@ -29,12 +29,19 @@ func init() {
 }
 
 func initConfig() {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Cannot get current directory, error:", err)
+		os.Exit(1)
+	}
+
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Cannot get user home directory, error:", err)
 		os.Exit(1)
 	}
 
+	viper.AddConfigPath(currentDir)
 	viper.AddConfigPath(filepath.Join(userHomeDir, ".gvm"))
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
