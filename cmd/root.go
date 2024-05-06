edaeaa8bcf62a04adfb44ed284eff86f7f79ce6d
rpanchyk <ruslan.panchik@gmail.com>
2024-05-06 16:54:28 +0300

    Remove current dir for config file search

--- cmd/root.go
+++ cmd/root.go
@@ -29,19 +29,12 @@ func init() {
 }
 
 func initConfig() {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Cannot get current directory, error:", err)
-		os.Exit(1)
-	}
-
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Cannot get user home directory, error:", err)
 		os.Exit(1)
 	}
 
-	viper.AddConfigPath(currentDir)
 	viper.AddConfigPath(filepath.Join(userHomeDir, ".gvm"))
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
